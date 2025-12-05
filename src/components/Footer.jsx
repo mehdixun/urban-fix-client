815d55a13ecb95a57e29a65b8154acd2f8dcfac6
mehdixun <mehdixhasn@gmail.com>
2025-12-05 21:25:13 +0600

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-base-200 text-center p-6 mt-20 shadow-inner">
+      <p>© 2025 UrbanFix. All rights reserved.</p>
+      <p>Designed by Mehdi Hasn</p>
+    </footer>
+  );
+};
+
+export default Footer;
